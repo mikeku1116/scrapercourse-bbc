c4de3dd8f56fe33599d9452b0243893eacf4f02b
mikeku1116 <mikekuforgit@gmail.com>
2022-03-10 23:25:00 +0800

    3.5批次爬取多分頁的網頁資料

--- scraper.py
+++ scraper.py
@@ -1,26 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(
-    'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt')
+for page in range(1, 4):
+    response = requests.get(
+        f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt/page/{page}')
 
-soup = BeautifulSoup(response.text, 'lxml')
-titles = soup.find_all(
-    'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
+    soup = BeautifulSoup(response.text, 'lxml')
+    titles = soup.find_all(
+        'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
 
-title_list = []
-for title in titles:
-    title_list.append(title.getText())
+    title_list = []
+    for title in titles:
+        title_list.append(title.getText())
 
-urls = soup.find_all(
-    'a', {'class': 'qa-heading-link lx-stream-post__header-link'})
+    urls = soup.find_all(
+        'a', {'class': 'qa-heading-link lx-stream-post__header-link'})
 
-tag_list = []
-for url in urls:
-    sub_response = requests.get('https://www.bbc.com' + url.get('href'))
-    sub_soup = BeautifulSoup(sub_response.text, 'lxml')
-    tags = sub_soup.find_all('li', {'class': 'bbc-1msyfg1 e1hq59l0'})
-    for tag in tags:
-        tag_list.append(tag.getText())
+    tag_list = []
+    for url in urls:
+        sub_response = requests.get('https://www.bbc.com' + url.get('href'))
+        sub_soup = BeautifulSoup(sub_response.text, 'lxml')
+        tags = sub_soup.find_all('li', {'class': 'bbc-1msyfg1 e1hq59l0'})
+        for tag in tags:
+            tag_list.append(tag.getText())
 
-print(tag_list)
+    print(f"第{page}頁")
+    print(title_list)
+    print(tag_list)
